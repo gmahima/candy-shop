95a4f6ea71fa403695b4d2b58a49d1dfa417f259
gmahima <mahimagangavarapu@gmail.com>
2020-12-27 12:36:45 +0530

    remove css files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
 import { GlobalStyles } from 'twin.macro'
 import styled, {css} from 'styled-components'
 import tw from 'twin.macro'
 
 const Container = styled.div`
 ${tw `
- bg-testing-100 min-h-screen text-white flex flex-col items-center justify-center
+ bg-gray-100 min-h-screen text-gray-700 flex flex-col items-center justify-center
 `}
 `
 function App() {
