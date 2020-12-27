c89240b00db7d84e97a4e2acc101e896b01d2233
gmahima <mahimagangavarapu@gmail.com>
2020-12-27 12:33:40 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { GlobalStyles } from 'twin.macro'
+import styled, {css} from 'styled-components'
+import tw from 'twin.macro'
 
+const Container = styled.div`
+${tw `
+ bg-testing-100 min-h-screen text-white flex flex-col items-center justify-center
+`}
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyles />
+      <Container>
+        <div>cra + twin.macro</div>
+    </Container>
     </div>
   );
 }
