3a97050d1621091d15e63ea101f9f915d5d80725
gmahima <mahimagangavarapu@gmail.com>
2020-12-30 11:47:20 +0530

    workinggit add .! wohoo

--- client/lib/index.js-e
+++ client/lib/index.js-e
@@ -10,10 +10,8 @@ var _App = _interopRequireDefault(require("./App"));
 
 var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
 
-var _reactRedux = require("react-redux");
-
-var _store = _interopRequireDefault(require("./redux/store"));
-
+// import {Provider} from 'react-redux'
+// import store from './redux/store'
 _reactDom.default.hydrate( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_App.default, null)), document.getElementById('root')); // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,36 @@
 import React from 'react'
-import { GlobalStyles } from 'twin.macro'
-import styled, {createGlobalStyle} from 'styled-components'
+// import { GlobalStyles } from 'twin.macro'
+// import styled, {createGlobalStyle} from 'styled-components'
 // import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
 // import NavBar from './components/NavBar'
 // import Home from './pages/Home'
 // import About from './pages/About'
 
-const Container = styled.div`
-`
-const GlobalStyleComponent = createGlobalStyle`
-body {
-  margin: 0;
-  padding: 0;
-  font-family: Open-Sans, Helvetica, Sans-Serif;
-  background: whitesmoke;
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  ul {
-    list-style-type: none;
-  }
-}
-`;
+// const Container = styled.div`
+// `
+// const GlobalStyleComponent = createGlobalStyle`
+// body {
+//   margin: 0;
+//   padding: 0;
+//   font-family: Open-Sans, Helvetica, Sans-Serif;
+//   background: whitesmoke;
+//   a {
+//     color: inherit;
+//     text-decoration: none;
+//   }
+//   ul {
+//     list-style-type: none;
+//   }
+// }
+// `;
 
 function App() {
   return (
     <>
       {/* // <Router> */}
-        <GlobalStyleComponent></GlobalStyleComponent>
+        {/* <GlobalStyleComponent></GlobalStyleComponent> */}
         {/* // <NavBar></NavBar> */}
-        <Container>
+        <div>
         {/* <Switch>
           <Route path="/about">
             <About/>
@@ -41,7 +41,7 @@ function App() {
         </Switch> */}
         {/* <Home/> */}
         hi
-        </Container>
+        </div>
       {/* </Router> */}
       </>
   );
